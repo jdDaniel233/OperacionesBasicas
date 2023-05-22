4572dde2c1935ffddd72e6f52010b50a10132226
Daniel <simbanadaniel42@gmail.com>
2023-05-22 18:05:11 -0500

    Programar eventos de los botones hasta suma

--- app/src/main/java/com/example/operacionesbasicas/MainActivity.java
+++ app/src/main/java/com/example/operacionesbasicas/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.operacionesbasicas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //METODO PARA CAMBIAR DE ACTIVIDAD
+    public void irSuma(View view){
+        Intent intent=new Intent(this,SumaActivity.class);
+        view.getContext().startActivity(intent);
     }
 }
--- app/src/main/java/com/example/operacionesbasicas/SumaActivity.java
+++ app/src/main/java/com/example/operacionesbasicas/SumaActivity.java
@@ -3,6 +3,9 @@ package com.example.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SumaActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class SumaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suma);
+    }
+
+    public void regresarMenu(View view){
+       this.finish();
+    }
+
+    public void sumar(View view) {
+        EditText numero1 = (EditText) findViewById(R.id.txtNumero1);
+        EditText numero2 = (EditText) findViewById(R.id.txtNumero2);
+        TextView total = findViewById(R.id.txtResultado);
+        double resultado = Double.parseDouble(numero1.getText().toString()) + Double.parseDouble(numero2.getText().toString());
+        total.setText(resultado+"");
     }
 }
