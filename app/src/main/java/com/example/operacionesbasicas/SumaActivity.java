089be7d4fb5aa8b050020202a564cd9238728647
Daniel <simbanadaniel42@gmail.com>
2023-05-22 17:19:37 -0500

    segunda pantalla SumaActivity

---
+++ app/src/main/java/com/example/operacionesbasicas/SumaActivity.java
@@ -0,0 +1,14 @@
+package com.example.operacionesbasicas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SumaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suma);
+    }
+}
