a196c62168495e77ea5ac5778e2944174de06153
Daniel <simbanadaniel42@gmail.com>
2023-05-24 20:47:18 -0500

    Diseño y programacion de DivisionActivity

--- app/src/main/java/com/example/operacionesbasicas/DivisionActivity.java
+++ app/src/main/java/com/example/operacionesbasicas/DivisionActivity.java
@@ -3,6 +3,9 @@ package com.example.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DivisionActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class DivisionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_division);
+    }
+    public void regresarMenu(View view){
+        this.finish();
+    }
+
+    public void division(View view) {
+        EditText numero1 = (EditText) findViewById(R.id.txtnum1);
+        EditText numero2 = (EditText) findViewById(R.id.txtnum2);
+        TextView total = findViewById(R.id.txtresult);
+        double resultado = Double.parseDouble(numero1.getText().toString()) / Double.parseDouble(numero2.getText().toString());
+        total.setText(resultado+"");
     }
 }
